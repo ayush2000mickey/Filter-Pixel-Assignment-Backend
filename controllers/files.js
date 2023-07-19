cf53087a0541931c0dc5d5715a98a17b07ac472b
Ayush Kumar <ayush2000mickey@gmail.com>
2023-07-19 19:21:33 +0530

    Some new changes

--- controllers/files.js
+++ controllers/files.js
@@ -9,8 +9,8 @@ const SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = path.join(process.cwd(), "token.json");
-const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
+// const TOKEN_PATH = path.join(process.cwd(), "token.json");
+// const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
 
 /**
  * Reads previously authorized credentials from the save file.
@@ -40,7 +40,7 @@ async function loadSavedCredentialsIfExist() {
  * @return {Promise<void>}
  */
 async function saveCredentials(client) {
-  const content = await fs.readFile(CREDENTIALS_PATH);
+  // const content = await fs.readFile(CREDENTIALS_PATH);
   // const keys = JSON.parse(content);
   const keys = {
     installed: {
@@ -60,7 +60,7 @@ async function saveCredentials(client) {
     client_secret: key.client_secret,
     refresh_token: client.credentials.refresh_token,
   });
-  await fs.writeFile(TOKEN_PATH, payload);
+  // await fs.writeFile(TOKEN_PATH, payload);
 }
 
 /**
@@ -72,14 +72,14 @@ async function authorize() {
   if (client) {
     return client;
   }
-  client = await authenticate({
-    scopes: SCOPES,
-    keyfilePath: CREDENTIALS_PATH,
-  });
-  if (client.credentials) {
-    await saveCredentials(client);
-  }
-  return client;
+  // client = await authenticate({
+  //   scopes: SCOPES,
+  //   keyfilePath: CREDENTIALS_PATH,
+  // });
+  // if (client.credentials) {
+  //   await saveCredentials(client);
+  // }
+  // return client;
 }
 
 /**
@@ -108,7 +108,7 @@ async function listFiles(authClient) {
   }
 }
 
-authorize().then(listFiles).catch(console.error);
+// authorize().then(listFiles).catch(console.error);
 
 const getDriveLinks = async (req, res) => {
   console.log("req recieved");
--- server.js
+++ server.js
@@ -15,21 +15,6 @@ app.use(cors());
 app.use("/drive", listFiles);
 app.use("/s3", readList);
 
-// const __dirname1 = path.resolve();
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "../frontend/build")));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(
-//       path.resolve(__dirname1, "../frontend", "build", "index.html")
-//     );
-//   });
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is Running Successfully");
-//   });
-// }
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server started on PORT ${port}`);
